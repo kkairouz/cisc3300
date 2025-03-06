109c10cf12d3549f60c8b8f675deac2830ece006
Kev <kev@Kevs-MacBook-Air.local>
2025-03-05 20:44:31 -0500

    modified homework

---
+++ homework6/problem8/index.php
@@ -0,0 +1,11 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>This is an image problem 8</title>
+</head>
+<body>    
+    <img src="resources/image.jpeg" style="max-width: 100%; height: auto;">
+</body>
+</html>
